5029d840586dd9320e7d70ae201e958c6ca9bd78
ktony-svg <ktony1472@gmail.com>
2025-02-26 14:22:22 +0300

    Update cafe.js

--- cafe.js
+++ cafe.js
@@ -1,6 +1,34 @@
-
 let userName = prompt("Please enter your first name:");
+
 if (userName === null || userName.trim() === "") {
     userName = "Guest";
 }
+
 alert("Welcome to EatNow Cafe, " + userName + "!");
+
+let menuItems = [
+    "Chicken Biryani",
+    "Vegetable Curry",
+    "Grilled Fish",
+    "Spaghetti Bolognese",
+    "Mango Lassi"
+];
+
+console.log("EatNow Cafe Menu:");
+menuItems.forEach(function(item, index) {
+    console.log((index + 1) + ". " + item);
+});
+
+let newDish = prompt("Suggest a new dish:");
+
+if (newDish !== null && newDish.trim() !== "") {
+    menuItems.push(newDish);
+    console.log("New dish added: " + newDish);
+
+    console.log("Updated EatNow Cafe Menu:");
+    menuItems.forEach(function(item, index) {
+        console.log((index + 1) + ". " + item);
+    });
+} else {
+    console.log("No new dish added.");
+}
